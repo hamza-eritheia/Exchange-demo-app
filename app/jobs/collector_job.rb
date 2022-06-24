973d8c365b45bcdfc17d5123dfc570d6e907e784
Hamza Awan <hamza.awan@eritheialabs.io>
2022-06-24 19:11:34 +0500

    Added cron job for collecting exchange rates

--- app/jobs/collector_job.rb
+++ app/jobs/collector_job.rb
@@ -1,7 +1,22 @@
+# frozen_string_literal: true
+
+# Background Job for collecting exchange rates
 class CollectorJob < ApplicationJob
   queue_as :urgent
 
-  def perform(*args)
-    # Do something later
+  include HTTParty
+  BASE_URL = 'http://api.nbp.pl/api/'
+  TABLE_TYPES = %w[A B C].freeze
+
+  def perform
+    TABLE_TYPES.each do |type|
+      resp = HTTParty.get("#{BASE_URL}/exchangerates/tables/#{type}/").parsed_response[0]
+
+      rates = resp.delete('rates')
+      table_type = TableType.create(resp)
+      table_type.rates.create(rates)
+    end
+  rescue => e
+    puts "Job failed: #{e}"
   end
 end
